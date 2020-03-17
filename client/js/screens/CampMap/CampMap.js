26947d2eca7170f4f74f06c540fec421c58880cb
ringo-w <ringo.sja@gmail.com>
2020-03-17 15:58:02 -0700

    Implement working Google Maps on iOS

--- client/js/screens/CampMap/CampMap.js
+++ client/js/screens/CampMap/CampMap.js
@@ -1,10 +1,30 @@
 import React from 'react';
-import {Text} from 'react-native';
+import {StyleSheet, View} from 'react-native';
+import MapView, {PROVIDER_GOOGLE} from 'react-native-maps'; // remove PROVIDER_GOOGLE import if not using Google Maps
 
-class CampMap extends React.Component {
-  render() {
-    return <Text>Map</Text>;
-  }
-}
+const styles = StyleSheet.create({
+  container: {
+    ...StyleSheet.absoluteFillObject,
+    height: 730,
+    width: 420,
+    justifyContent: 'flex-end',
+    alignItems: 'center',
+  },
+  map: {
+    ...StyleSheet.absoluteFillObject,
+  },
+});
 
-export default CampMap;
+export default () => (
+  <View style={styles.container}>
+    <MapView
+      provider={PROVIDER_GOOGLE} // remove if not using Google Maps
+      style={styles.map}
+      region={{
+        latitude: 49.26335,
+        longitude: -123.13703,
+        latitudeDelta: 0.0922,
+        longitudeDelta: 0.0421,
+      }}></MapView>
+  </View>
+);
