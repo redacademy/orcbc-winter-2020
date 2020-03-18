3afc3618a884f263c92e3f062f88de07b4676b74
ringo-w <ringo.sja@gmail.com>
2020-03-18 11:37:03 -0700

    Add dark mode, myLocation, SearchBar (UI only)

--- client/js/screens/CampMap/CampMap.js
+++ client/js/screens/CampMap/CampMap.js
@@ -1,217 +1,51 @@
 import React from 'react';
-import {StyleSheet, View} from 'react-native';
-import MapView, {PROVIDER_GOOGLE} from 'react-native-maps'; // remove PROVIDER_GOOGLE import if not using Google Maps
+import {View, TextInput} from 'react-native';
+import MapView, {PROVIDER_GOOGLE} from 'react-native-maps';
+import MapStyle from './MapStyle';
+import styles from './styles';
 
-MapStyle = [
-  {
-    elementType: 'geometry',
-    stylers: [
-      {
-        color: '#212121',
-      },
-    ],
-  },
-  {
-    elementType: 'labels.icon',
-    stylers: [
-      {
-        visibility: 'off',
-      },
-    ],
-  },
-  {
-    elementType: 'labels.text.fill',
-    stylers: [
-      {
-        color: '#757575',
-      },
-    ],
-  },
-  {
-    elementType: 'labels.text.stroke',
-    stylers: [
-      {
-        color: '#212121',
-      },
-    ],
-  },
-  {
-    featureType: 'administrative',
-    elementType: 'geometry',
-    stylers: [
-      {
-        color: '#757575',
-      },
-    ],
-  },
-  {
-    featureType: 'administrative.country',
-    elementType: 'labels.text.fill',
-    stylers: [
-      {
-        color: '#9e9e9e',
-      },
-    ],
-  },
-  {
-    featureType: 'administrative.land_parcel',
-    stylers: [
-      {
-        visibility: 'off',
-      },
-    ],
-  },
-  {
-    featureType: 'administrative.locality',
-    elementType: 'labels.text.fill',
-    stylers: [
-      {
-        color: '#bdbdbd',
-      },
-    ],
-  },
-  {
-    featureType: 'poi',
-    elementType: 'labels.text.fill',
-    stylers: [
-      {
-        color: '#757575',
-      },
-    ],
-  },
-  {
-    featureType: 'poi.park',
-    elementType: 'geometry',
-    stylers: [
-      {
-        color: '#181818',
-      },
-    ],
-  },
-  {
-    featureType: 'poi.park',
-    elementType: 'labels.text.fill',
-    stylers: [
-      {
-        color: '#616161',
-      },
-    ],
-  },
-  {
-    featureType: 'poi.park',
-    elementType: 'labels.text.stroke',
-    stylers: [
-      {
-        color: '#1b1b1b',
-      },
-    ],
-  },
-  {
-    featureType: 'road',
-    elementType: 'geometry.fill',
-    stylers: [
-      {
-        color: '#2c2c2c',
-      },
-    ],
-  },
-  {
-    featureType: 'road',
-    elementType: 'labels.text.fill',
-    stylers: [
-      {
-        color: '#8a8a8a',
-      },
-    ],
-  },
-  {
-    featureType: 'road.arterial',
-    elementType: 'geometry',
-    stylers: [
-      {
-        color: '#373737',
-      },
-    ],
-  },
-  {
-    featureType: 'road.highway',
-    elementType: 'geometry',
-    stylers: [
-      {
-        color: '#3c3c3c',
-      },
-    ],
-  },
-  {
-    featureType: 'road.highway.controlled_access',
-    elementType: 'geometry',
-    stylers: [
-      {
-        color: '#4e4e4e',
-      },
-    ],
-  },
-  {
-    featureType: 'road.local',
-    elementType: 'labels.text.fill',
-    stylers: [
-      {
-        color: '#616161',
-      },
-    ],
-  },
-  {
-    featureType: 'transit',
-    elementType: 'labels.text.fill',
-    stylers: [
-      {
-        color: '#757575',
-      },
-    ],
-  },
-  {
-    featureType: 'water',
-    elementType: 'geometry',
-    stylers: [
-      {
-        color: '#000000',
-      },
-    ],
-  },
-  {
-    featureType: 'water',
-    elementType: 'labels.text.fill',
-    stylers: [
-      {
-        color: '#3d3d3d',
-      },
-    ],
-  },
-];
-const styles = StyleSheet.create({
-  container: {
-    ...StyleSheet.absoluteFillObject,
-    height: 730,
-    width: 420,
-    justifyContent: 'flex-end',
-    alignItems: 'center',
-  },
-  map: {
-    ...StyleSheet.absoluteFillObject,
-  },
-});
-
-export default () => (
-  <View style={styles.container}>
-    <MapView
-      customMapStyle={MapStyle}
-      provider={PROVIDER_GOOGLE} // remove if not using Google Maps
-      style={styles.map}
-      region={{
-        latitude: 49.26335,
-        longitude: -123.13703,
-        latitudeDelta: 0.0922,
-        longitudeDelta: 0.0421,
-      }}></MapView>
-  </View>
-);
+export default () => {
+  const [value, onChangeText] = React.useState('');
+  return (
+    <View style={styles.container}>
+      <View
+        style={{
+          postion: 'absolute',
+          top: '5%',
+          width: '100%',
+          zIndex: 1,
+        }}>
+        <View
+          style={{
+            backgroundColor: 'white',
+            width: '90%',
+            height: '22%',
+            alignSelf: 'center',
+            justifyContent: 'center',
+          }}>
+          <TextInput
+            style={{
+              fontSize: 20,
+              paddingLeft: 30,
+            }}
+            placeholder="Search"
+            onChangeText={text => onChangeText(text)}
+            value={value}
+          />
+        </View>
+      </View>
+      <MapView
+        customMapStyle={MapStyle}
+        showsMyLocationButton={true}
+        showsUserLocation={true}
+        provider={PROVIDER_GOOGLE}
+        style={styles.map}
+        region={{
+          latitude: 49.26335,
+          longitude: -123.13703,
+          latitudeDelta: 0.0922,
+          longitudeDelta: 0.0421,
+        }}></MapView>
+    </View>
+  );
+};
---
+++ client/js/screens/CampMap/styles.js
@@ -0,0 +1,15 @@
+import {StyleSheet} from 'react-native';
+
+const styles = StyleSheet.create({
+  container: {
+    ...StyleSheet.absoluteFillObject,
+    height: 730,
+    width: 420,
+    alignItems: 'center',
+  },
+  map: {
+    ...StyleSheet.absoluteFillObject,
+  },
+});
+
+export default styles;
