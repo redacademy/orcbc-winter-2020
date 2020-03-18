0dcb724a751a897a7ff01680fe16d6e575719d10
ringo-w <ringo.sja@gmail.com>
2020-03-18 14:00:40 -0700

    Dismiss keyboard when clicking away from Search box

--- client/js/screens/CampMap/CampMap.js
+++ client/js/screens/CampMap/CampMap.js
@@ -1,51 +1,44 @@
-import React from 'react';
-import {View, TextInput} from 'react-native';
+import React, {useState} from 'react';
+import {
+  View,
+  TextInput,
+  Keyboard,
+  TouchableWithoutFeedback,
+} from 'react-native';
 import MapView, {PROVIDER_GOOGLE} from 'react-native-maps';
 import MapStyle from './MapStyle';
 import styles from './styles';
 
 export default () => {
-  const [value, onChangeText] = React.useState('');
+  const [value, onChangeText] = useState('');
+  const [region, setRegion] = useState({
+    latitude: 49.26335,
+    longitude: -123.13703,
+    latitudeDelta: 0.0922,
+    longitudeDelta: 0.0421,
+  });
+
   return (
-    <View style={styles.container}>
-      <View
-        style={{
-          postion: 'absolute',
-          top: '5%',
-          width: '100%',
-          zIndex: 1,
-        }}>
-        <View
-          style={{
-            backgroundColor: 'white',
-            width: '90%',
-            height: '22%',
-            alignSelf: 'center',
-            justifyContent: 'center',
-          }}>
-          <TextInput
-            style={{
-              fontSize: 20,
-              paddingLeft: 30,
-            }}
-            placeholder="Search"
-            onChangeText={text => onChangeText(text)}
-            value={value}
-          />
+    <TouchableWithoutFeedback onPress={() => Keyboard.dismiss()}>
+      <View style={styles.container}>
+        <View style={styles.searchBox}>
+          <View style={styles.search}>
+            <TextInput
+              style={styles.text}
+              placeholder="Search"
+              onChangeText={text => onChangeText(text)}
+              value={value}
+            />
+          </View>
         </View>
+        <MapView
+          customMapStyle={MapStyle}
+          showsMyLocationButton={true}
+          showsUserLocation={true}
+          provider={PROVIDER_GOOGLE}
+          style={styles.map}
+          initialRegion={region}></MapView>
       </View>
-      <MapView
-        customMapStyle={MapStyle}
-        showsMyLocationButton={true}
-        showsUserLocation={true}
-        provider={PROVIDER_GOOGLE}
-        style={styles.map}
-        region={{
-          latitude: 49.26335,
-          longitude: -123.13703,
-          latitudeDelta: 0.0922,
-          longitudeDelta: 0.0421,
-        }}></MapView>
-    </View>
+    </TouchableWithoutFeedback>
   );
 };
