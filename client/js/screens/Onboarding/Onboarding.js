56e01b0e8f833442c6c3d3d1e1bf5a5ed15842b6
Brooke <brooke.xiang@gmail.com>
2020-03-18 14:42:46 -0700

    change onboarding to combine changes

--- client/js/screens/Onboarding/Onboarding.js
+++ client/js/screens/Onboarding/Onboarding.js
@@ -1,15 +1,19 @@
 import React from 'react';
 import Onboarding from 'react-native-onboarding-swiper';
-import {Text, ImageBackground, Image, ScrollView, View} from 'react-native';
+import {Text, ImageBackground, ScrollView, View} from 'react-native';
 import MapBackground from '../../assets/P3_ORC_ImagePool/map.jpg';
+import LookingOut from '../../assets/P3_ORC_ImagePool/lookingout.jpg';
+import HikingPeople from '../../assets/P3_ORC_ImagePool/hikingpeople.jpg';
+import FlatLay from '../../assets/P3_ORC_ImagePool/flatlay.jpg';
+import Marshmallow from '../../assets/P3_ORC_ImagePool/firemarshmallow.jpg';
+import OnboardingComp from '../../components/OnboardingComp';
 
-const Triangle = ({isLight, selected}) => {
+const Triangle = ({selected}) => {
   let backgroundColor;
-  // if(isLight) {
   backgroundColor = selected ? 'green' : 'grey';
-  // } else {
-  //   background
-  // }
+  position = 'absolute';
+  top = 400;
+
   return (
     <View
       style={{
@@ -28,93 +32,87 @@ const Triangle = ({isLight, selected}) => {
   );
 };
 
-const Onboard = () => {
+const Onboard = props => {
+  const {navigation} = props;
   return (
-    <ScrollView>
-      <Onboarding
-        DotComponent={Triangle}
-        imageContainerStyles={{paddingBottom: 0}}
-        pages={[
-          {
-            backgroundColor: '#fff',
-            image: (
-              <ImageBackground
-                source={MapBackground}
-                style={{width: '100%', height: '100%'}}>
-                <View
-                  style={{
-                    marginTop: 300,
-                    backgroundColor: 'white',
-                    opacity: 0.75,
-                    alignItems: 'center',
-                    justifyItems: 'center',
-                  }}>
-                  <Text>Map</Text>
-                  <Text>
-                    Find everything you need from campsites, fuel station,
-                    hospital, and more.
-                  </Text>
-                </View>
-              </ImageBackground>
-            ),
-            title: 'Map',
-            subtitle:
-              'Find everything you need from campsites, fuel station, hospital, and more.',
-            // titleStyles: {position: 'absolute'},
-          },
-          {
-            backgroundColor: '#fff',
-            image: (
-              <Image
-                source={require('../../assets/P3_ORC_ImagePool/Lake1.jpeg')}
-                style={{height: '100%', width: '100%', opacity: 1}}
-              />
-            ),
-            title: 'Trips',
-            subtitle:
-              'Keep records of your past and upcoming camping reservations.',
-          },
-          {
-            backgroundColor: '#fff',
-            image: (
-              <Image
-                source={require('../../assets/P3_ORC_ImagePool/Lake1.jpeg')}
-                style={{height: '100%', width: '100%', opacity: 1}}
-              />
-            ),
-            title: 'Community',
-            subtitle:
-              'Post about or upload a picture to share your recent exciting camping experience with the community.',
-          },
-          {
-            backgroundColor: '#fff',
-            image: (
-              <Image
-                source={require('../../assets/P3_ORC_ImagePool/Lake1.jpeg')}
-                style={{height: '100%', width: '100%', opacity: 1}}
-              />
-            ),
-            title: 'Camping Guide',
-            subtitle:
-              'Make your camping expeirnece much easier with guides for emergencies, first-aid, camping equipments, etc.',
-            containerStyles: {backgroundColor: 'red'},
-          },
-          {
-            backgroundColor: '#fff',
-            image: (
-              <Image
-                source={require('../../assets/P3_ORC_ImagePool/Lake1.jpeg')}
-                style={{height: '100%', width: '100%', opacity: 1}}
-              />
-            ),
-            title: 'Leave no Trace',
-            subtitle:
-              '1) Plan ahead and prepare 2) Travel and camp on durable surfaces 3) Dispose of waste properly 4) Leave what you find 5) Minimize campfire impacts 6) Respect wildlife 7) Be considerate of others',
-            containerStyles: {backgroundColor: 'red'},
-          },
-        ]}
-      />
-    </ScrollView>
+    // <ScrollView>
+    <Onboarding
+      DotComponent={Triangle}
+      // onSkip={}
+      showNext={false}
+      bottomBarHighlight={false}
+      imageContainerStyles={{paddingBottom: 0}}
+      pages={[
+        {
+          backgroundColor: '#fff',
+          image: (
+            <OnboardingComp
+              img={MapBackground}
+              title="Map"
+              text="Find everything you need from campsites, fuel station, hospital, and more."
+            />
+          ),
+          // title: '',
+          // subtitle: '',
+          // titleStyles: {display: 'none'},
+          // subtitleStyles: {display: 'none'},
+        },
+        {
+          backgroundColor: '#fff',
+          image: (
+            <OnboardingComp
+              img={LookingOut}
+              title="Trips"
+              text="Keep records of your past and upcoming camping reservations.
+                "
+            />
+          ),
+        },
+        {
+          backgroundColor: '#fff',
+          image: (
+            <OnboardingComp
+              img={HikingPeople}
+              title="Community"
+              text="Post about or upload a picture to share your recent exciting camping experience with the community."
+            />
+          ),
+        },
+        {
+          backgroundColor: '#fff',
+          image: (
+            <OnboardingComp
+              img={FlatLay}
+              title="Camping Guide"
+              text="Make your camping expeirnece much easier with guides for emergencies, first-aid, camping equipments, etc."
+            />
+          ),
+        },
+        {
+          backgroundColor: '#fff',
+          image: (
+            <OnboardingComp
+              img={Marshmallow}
+              title="Leave no Trace"
+              text={
+                <>
+                  <Text>1. Plan ahead and prepare</Text>
+                  <Text>2. Travel and camp on durable surfaces</Text>
+                  <Text>3. Dispose of waste properly</Text>
+                  <Text>4. Leave what you find</Text>
+                  <Text>5. Minimize campfire impacts</Text>
+                  <Text>6. Respect wildlife</Text>
+                  <Text>7. Be considerate of others</Text>
+                </>
+              }
+            />
+          ),
+          title: '',
+          subtitle: '',
+        },
+      ]}
+    />
+    // </ScrollView>
   );
 };
 
