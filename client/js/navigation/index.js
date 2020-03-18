0e38de53d7226d49bcc0f2553821227a58e76fb5
Brooke <brooke.xiang@gmail.com>
2020-03-18 11:20:02 -0700

    add auth flow for onboarding

--- client/js/navigation/index.js
+++ client/js/navigation/index.js
@@ -1,18 +1,26 @@
 import React from 'react';
 import Navigation from './navigation';
+import OnBoarding from '../screens/Onboarding';
 import Cancel from '../screens/Cancel';
 import Calendar from '../screens/Calendar';
 import ParkInfo from '../screens/ParkInfo';
 import {createStackNavigator} from '@react-navigation/stack';
+import {State} from 'react-native-gesture-handler';
 
 const RootStack = createStackNavigator();
 
 const RootStackScreens = props => (
   <RootStack.Navigator mode="modal" headerMode="none">
-    <RootStack.Screen name="Main" component={Navigation} />
-    <RootStack.Screen name="ParkInfo" component={ParkInfo} />
-    <RootStack.Screen name="Calendar" component={Calendar} />
-    <RootStack.Screen name="Cancel" component={Cancel} />
+    {State.userToken == null ? (
+      <RootStack.Screen name="OnBoarding" component={OnBoarding} />
+    ) : (
+      <>
+        <RootStack.Screen name="Main" component={Navigation} />
+        <RootStack.Screen name="ParkInfo" component={ParkInfo} />
+        <RootStack.Screen name="Calendar" component={Calendar} />
+        <RootStack.Screen name="Cancel" component={Cancel} />
+      </>
+    )}
   </RootStack.Navigator>
 );
 
