4be0040b49d83bbfcbb2c79650c6315018cf9169
Brooke <brooke.xiang@gmail.com>
2020-03-20 12:28:32 -0700

    add styling to onboarding component

--- client/js/components/OnboardingComp/OnboardingComp.js
+++ client/js/components/OnboardingComp/OnboardingComp.js
@@ -1,26 +1,14 @@
 import React from 'react';
 import CText from '../CustomText';
 import {ImageBackground, View} from 'react-native';
+import styles from './styles';
 
 const OnboardingComp = ({img, title, text}) => {
   return (
-    <ImageBackground
-      source={img}
-      style={{width: '100%', height: '100%', opacity: 0.75}}>
-      <View
-        style={{
-          marginTop: 500,
-          width: '70%',
-          backgroundColor: 'white',
-          alignSelf: 'center',
-          opacity: 0.75,
-          padding: 30,
-          alignItems: 'center',
-          justifyItems: 'center',
-          borderRadius: 10,
-        }}>
-        <CText>{title}</CText>
-        <CText>{text}</CText>
+    <ImageBackground source={img} style={styles.background}>
+      <View style={styles.textContainer}>
+        <CText style={styles.title}>{title}</CText>
+        <CText style={styles.text}>{text}</CText>
       </View>
     </ImageBackground>
   );
