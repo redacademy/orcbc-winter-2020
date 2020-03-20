154aca8bf8cb8ed16287c9cacec912f9d6986546
ringo-w <ringo.sja@gmail.com>
2020-03-20 13:25:30 -0700

    Add more text

--- client/js/screens/Safety/Safety.js
+++ client/js/screens/Safety/Safety.js
@@ -1,17 +1,19 @@
 import React from 'react';
-import {SafeAreaView, View, Text, Image} from 'react-native';
+import {SafeAreaView, View, Text, ImageBackground} from 'react-native';
 
 const Safety = () => {
   return (
     <SafeAreaView>
-      <Image source={require('../../assets/Guide/images/avalanche.jpg')} />
-
+      <ImageBackground
+        source={require('../../assets/Guide/images/avalanche.jpg')}
+        style={{width: '100%', height: '100%', flex: 1}}
+      />
       <View
         style={{
           display: 'flex',
           alignItems: 'center',
           marginTop: 55,
-          zIndex: 2,
+          zIndex: 1,
         }}>
         <Text style={{fontSize: 24, fontFamily: 'Montserrat'}}>
           Avalanche Safety
@@ -24,7 +26,30 @@ const Safety = () => {
           borderRadiusRight: 50,
           backgroundColor: 'white',
           zIndex: 1,
-        }}></View>
+          borderTopLeftRadius: 50,
+          borderTopRightRadius: 50,
+          height: '100%',
+        }}>
+        <Text
+          style={{
+            fontSize: 20,
+            paddingTop: 50,
+            textDecorationLine: 'underline',
+          }}>
+          Quick Tips
+        </Text>
+        <Text style={{fontSize: 16, color: '#343434'}}>
+          If you become caught in an avalanche, try to:
+        </Text>
+        <Text>
+          Push machinery, equipment or heavy objects away from you to avoid
+          injury. {'\n\n'}Grab onto anything solid (trees, rocks, etc.) to avoid
+          being swept away. {'\n\n'}Keep your mouth closed and your teeth
+          clenched. {'\n\n'}If you start moving downward with the avalanche,
+          stay on the surface using a swimming motion. {'\n\n'}Try to move
+          yourself to the side of the avalanche.
+        </Text>
+      </View>
     </SafeAreaView>
   );
 };
