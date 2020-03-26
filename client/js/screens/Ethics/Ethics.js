c70858a5150a64dbbb5747d0e300cc4028e997e1
Brooke <brooke.xiang@gmail.com>
2020-03-26 13:07:25 -0700

    add outdoor ethics screen

--- client/js/screens/Ethics/Ethics.js
+++ client/js/screens/Ethics/Ethics.js
@@ -1,8 +1,36 @@
 import React from 'react';
 import CText from '../../components/CustomText';
+import PageLayout from '../../components/PageLayout';
+import EthicRule from '../../components/EthicRule';
+import outdoorEthics from './outdoorEthics.json';
+import styles from './styles';
 
-const Ethics = () => {
-  return <CText>Outdoor Ethics</CText>;
+const Ethics = ({navigation}) => {
+  {
+    console.log(outdoorEthics.rules);
+  }
+  return (
+    <PageLayout
+      imageSource={require('../../assets/Guide/images/avalanche.png')}
+      title="Outdoor Ethics"
+      onPress={() => {
+        navigation.goBack();
+      }}
+      arrowDisplay={{display: 'flex'}}
+      pageContent={
+        <>
+          <CText style={styles.title}>Principles of Outdoor Ethics</CText>
+          {outdoorEthics.rules.map(rule => (
+            <EthicRule
+              title={`${rule.id}. ${rule.title}`}
+              content={rule.content}
+              key={rule.id}
+            />
+          ))}
+        </>
+      }
+    />
+  );
 };
 
 export default Ethics;
