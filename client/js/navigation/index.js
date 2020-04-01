183d4b2de5533f1febd5da0ce4c9ef49b525e645
ringo-w <ringo.sja@gmail.com>
2020-04-01 11:48:31 -0700

    Fix index.js

--- client/js/navigation/index.js
+++ client/js/navigation/index.js
@@ -7,8 +7,6 @@ import Cancel from '../screens/Cancel';
 import Calendar from '../screens/Calendar';
 import ParkInfo from '../screens/ParkInfo';
 
-import PaymentMethod from '../screens/PaymentMethod';
-
 import {createStackNavigator} from '@react-navigation/stack';
 import {AuthContext} from '../context/AuthProvider';
 const AuthStack = createStackNavigator();
@@ -35,7 +33,6 @@ const RootStackScreens = props => {
             </>
           ) : (
             <>
-              <RootStack.Screen name="Payment" component={PaymentMethod} />
               <RootStack.Screen name="Main" component={App} />
               <RootStack.Screen name="ParkInfo" component={ParkInfo} />
               <RootStack.Screen name="Calendar" component={Calendar} />
